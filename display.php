8488d75080180b69d0d0aad61a515130a4138017
LordChakshu <chakshukota74@gmail.com>
2023-03-28 21:09:05 +0530

    Add CSS for metabox display and gravatar positioning

--- display.php
+++ display.php
@@ -7,7 +7,7 @@ function display_contributors_box( $content ) {
         return $content;
     }
     // Start building the contributors box HTML
-    $html = '<div class="contributors-box">';
+    $html = '<div class="contributors-box">'; 
     $html .= '<h3>Contributors</h3>';
     $html .= '<ul>';
     // Loop through the contributors and display their names and Gravatars
@@ -18,8 +18,8 @@ function display_contributors_box( $content ) {
             $html .= '<a href="' . get_author_posts_url( $contributor_id ) . '">' . get_avatar( $contributor_id, 35 ) .  '</a>';
             $html .= '</br>';
             $html .= '<div class="contributor-name">';
-            $html .= '<a href="' . get_author_posts_url( $contributor_id ) . '"> <button class="button">' .esc_html( $contributor->display_name ) . '</button></a>';
-            $html .= '</div>';
+            $html .= '<a href="' . get_author_posts_url( $contributor_id ) . '"> <button class="button button-1">' .esc_html( $contributor->display_name ) . '</button></a>';
+            $html .= '</div>';            
             $html .= '</li>';
         }
     }
@@ -32,7 +32,9 @@ function display_contributors_box( $content ) {
 
 function my_plugin_enqueue_styles() {
     wp_enqueue_style( 'my-plugin-style', plugins_url( '/css/button.css', __FILE__ ) );
+    wp_enqueue_style( 'my-plugin-box-style', plugins_url( '/css/box.css', __FILE__ ) );
 }
+
 add_action( 'wp_enqueue_scripts', 'my_plugin_enqueue_styles' );
 
 add_filter( 'the_content', 'display_contributors_box' );
